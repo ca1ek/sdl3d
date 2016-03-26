b293e1ffe14fdd6f85453707957c7c61ad7f617f
Dawid Pietrzak <ca1ekowy@gmail.com>
2016-03-26 19:28:26 +0100

    Made triangle test compatible with the current library

--- examples/triangle_test.rs
+++ examples/triangle_test.rs
@@ -1,6 +1,4 @@
-extern crate sdl2;
-extern crate sdl2_sys;
-extern crate libc;
+extern crate orbclient;
 
 extern crate sdl3d;
 
@@ -8,31 +6,28 @@ use sdl3d::vid::*;
 use sdl3d::start;
 
 fn main() {
-    use sdl2::pixels::Color::RGB;
 
-    let mut engine = start::Engine::new(1280, 720, "Hello!".to_string(), 1 as usize);
+    let mut engine = start::Engine::new(1280, 720, "Hello!", 1 as usize);
 
-    engine.renderer.set_draw_color(RGB(20, 40, 60));
-    engine.renderer.clear();
-    engine.renderer.set_draw_color(RGB(200, 200, 200));
+    engine.window.set(Color::new(20, 40, 60).orb_color());
 
     let point1 = DepthPoint::new(0.0, -0.5, 2.0);
     let point2 = DepthPoint::new(0.5, 0.5, 2.0);
     let point3 = DepthPoint::new(-0.5, 0.5, 2.0);
 
-    let triangle = Triangle::new(point1, point2, point3, 0.0, 0.0, 0.0);
+    let triangle = Triangle::new(point1, point2, point3, 0.0, 0.0, 0.0, Color::new(200, 200, 200));
 
     'game_loop: loop {
+        engine.window.set(Color::new(20, 40, 60).orb_color());
+
+
         engine.camera_z += 0.01;
-        engine.renderer.set_draw_color(RGB(20, 40, 60));
-        engine.renderer.clear();
-        engine.renderer.set_draw_color(RGB(200, 200, 200));
 
         engine.render_queue.push(triangle);
 
         engine.render();
 
-        engine.renderer.present();
+        engine.window.sync();
         std::thread::sleep(std::time::Duration::from_millis(33));
     }
 }
