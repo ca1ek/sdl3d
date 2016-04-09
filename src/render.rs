8e0f365486c98d221cb75b9bcd9c0a8298f9ed9a
ca1ek <ca1ek@users.noreply.github.com>
2016-04-09 17:03:25 +0200

    Added textures

--- src/render.rs
+++ src/render.rs
@@ -19,6 +19,58 @@ impl Color {
     pub fn as_orbclient(&self) -> orbclient::color::Color {
         orbclient::color::Color::rgb(self.r, self.g, self.b)
     }
+
+    pub fn from_orbclient(color: orbclient::color::Color) -> Color {
+        let r = (color.data & 0x00FF0000) >> 16;
+        let g = (color.data & 0x0000FF00) >> 8;
+        let b = color.data & 0x000000FF;
+
+        Color::new(r as u8, g as u8, b as u8)
+    }
+}
+
+pub struct Texture {
+    pub width: usize,
+    pub height: usize,
+    frame: Vec<Color>,
+}
+
+impl Texture {
+    pub fn new(w: usize, h: usize) -> Texture {
+        let mut tex: Vec<Color> = Vec::new();
+
+        for _ in 0..w*h {
+            tex.push(Color::new(0, 0, 0));
+        }
+
+        Texture {
+            width: w,
+            height: h,
+            frame: tex,
+        }
+    }
+
+    pub fn load(path: &str) -> Texture {
+        use std::ops::Deref;
+
+        let bmp = orbclient::bmp::BmpFile::from_path(path);
+        let w = bmp.width();
+        let h = bmp.height();
+
+        let orb_color_vec = bmp.deref();
+
+        let mut color_vec: Vec<Color> = Vec::new();
+
+        for i in 0..orb_color_vec.len() {
+            color_vec.push(Color::from_orbclient(orb_color_vec[i]));
+        }
+
+        Texture {
+            width: w,
+            height: h,
+            frame: color_vec,
+        }
+    }
 }
 
 pub struct Framebuffer {
