4274609cc057f01f15d3541d4fbae113f4c6e463
ca1ek <ca1ek@users.noreply.github.com>
2016-04-09 17:02:00 +0200

    Added Points.

--- src/geometry.rs
+++ src/geometry.rs
@@ -4,8 +4,25 @@ pub struct Point {
     z: f32,
 }
 
+impl Point {
+    pub fn make_2d(&self) -> FlatPoint {
+        FlatPoint {
+            x: self.x / self.z,
+            y: self.y / self.z,
+        }
+    }
+}
+
+pub struct FlatPoint {
+    x: f32,
+    y: f32,
+}
+
 pub struct Triangle {
     p1: Point,
     p2: Point,
     p3: Point,
+    uv_p1: FlatPoint,
+    uv_p2: FlatPoint,
+    uv_p3: FlatPoint,
 }
