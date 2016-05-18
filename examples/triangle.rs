99a5b8bcde8ffb6a8756b46571df8cc198860b38
ca1ek <ca1ek@users.noreply.github.com>
2016-05-18 21:14:33 +0200

    Added filled triangle example

--- examples/triangle.rs
+++ examples/triangle.rs
@@ -4,7 +4,12 @@ extern crate tetrahedrane;
 fn main() {
     let mut window = orbclient::window::Window::new_flags(-1, -1, 640, 480, &"dziki wunsz", true).unwrap();
     
+    let mut framecounter = 0;
+    let mut timecounter = std::time::Duration::new(0,0);
+    
     'gameloop: loop {
+        let begin = std::time::Instant::now();
+        
         for event in window.events() {
             use orbclient::event::EventOption;
             let ev_opt = event.to_option();
@@ -17,12 +22,23 @@ fn main() {
         
         window.clear();
         
-        tetrahedrane::renderers::wireframe::triangle_p(0.0, 0.0, 1.0,
+        tetrahedrane::renderers::filled::triangle_p(0.0, 0.0, 1.0,
                                                     1.0, 1.0, 1.0,
                                                     1.0, 0.0, 1.0, 
                                                     orbclient::Color::rgb(255,0,255),
                                                     &mut window);
         
         window.sync();
+        
+        let end = std::time::Instant::now();
+        
+        framecounter += 1;
+        timecounter += end.duration_since(begin);
+        
+        if timecounter > std::time::Duration::new(1, 0) {
+            timecounter = std::time::Duration::new(0, 0);
+            println!("FPS: {}", framecounter);
+            framecounter = 0;
+        }
     }
 }
