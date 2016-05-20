a14abfbbac2ac0623c01dd60f1338ba4e7b82854
ca1ek <ca1ek@users.noreply.github.com>
2016-05-20 23:40:26 +0200

    Added a point initializer

--- src/geometry.rs
+++ src/geometry.rs
@@ -1,7 +1,17 @@
-pub struct Point{
+pub struct Point {
     pub x: f32,
     pub y: f32,
     pub z: f32,
+}
+
+impl Point {
+    pub fn new(x: f32, y: f32, z: f32) -> Self {
+        Point {
+            x: x,
+            y: y,
+            z: z,
+        }
+    }
 }
 
 pub struct Triangle {
