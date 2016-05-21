3816d99938d3c504207f18d50c1426e8df3624d7
ca1ek <ca1ek@users.noreply.github.com>
2016-05-21 11:24:08 +0200

    Fix variable shadowing in rotation function

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -9,10 +9,10 @@ fn rotate(x: f32, y: f32, around_x: f32, around_y: f32, angle: f32) -> (f32, f32
     let x = x - around_x;
     let y = y - around_y;
     
-    let x = x * c - y * s;
-    let y = x * s + y * c;
+    let new_x = x * c - y * s;
+    let new_y = x * s + y * c;
     
-    (x + around_x, y + around_y)
+    (new_x + around_x, new_y + around_y)
 }
 
 pub trait Rotation {
@@ -22,26 +22,21 @@ pub trait Rotation {
 }
 
 impl Rotation for Point {
-    /// Rotates on x and y axes. You shouldn't rotate a point you rotated
-    /// earlier, instead keep rotating the initial one.
-    /// For some reason, it breaks for some reason if you keep rotating too much
-    fn rotate_x_y(&mut self, x: f32, y: f32, angle: f32) -> Point {
+    /// Rotates on x and y axes.
+    fn rotate_x_y(&mut self, x: f32, y: f32, angle: f32) {
         let (x, y) = rotate(self.x, self.y, x, y, angle);
-        Point {
-            x: x,
-            y: y,
-            z: self.z,
-        }
+        self.x = x;
+        self.y = y;
     }
     
-    /// Rotate on x and z axes. Same error as with rotate_x_y
+    /// Rotate on x and z axes.
     fn rotate_x_z(&mut self, x: f32, z: f32, angle: f32) {
         let (x, z) = rotate(self.x, self.z, x, z, angle);
         self.x = x;
         self.z = z;
     }
     
-    /// Rotate on y and z axes. Same error as with rotate_x_y
+    /// Rotate on y and z axes.
     fn rotate_y_z(&mut self, y: f32, z: f32, angle: f32) {
         let (y, z) = rotate(self.y, self.z, y, z, angle);
         self.y = y;
@@ -50,18 +45,21 @@ impl Rotation for Point {
 }
 
 impl Rotation for Triangle {
+    /// Rotates on x and y axes.
     fn rotate_x_y(&mut self, x: f32, y: f32, angle: f32) {
         self.p1.rotate_x_y(x, y, angle);
         self.p2.rotate_x_y(x, y, angle);
         self.p3.rotate_x_y(x, y, angle);
     }
     
+    /// Rotate on x and z axes.
     fn rotate_x_z(&mut self, x: f32, z: f32, angle: f32) {
         self.p1.rotate_x_z(x, z, angle);
         self.p2.rotate_x_z(x, z, angle);
         self.p3.rotate_x_z(x, z, angle);
     }
     
+    /// Rotate on y and z axes.
     fn rotate_y_z(&mut self, y: f32, z: f32, angle: f32) {
         self.p1.rotate_y_z(y, z, angle);
         self.p2.rotate_y_z(y, z, angle);
